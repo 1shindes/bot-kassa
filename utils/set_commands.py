0c886a619ce10640abe16a24d8fb5b6b7fbfe2d2
My Name <me@email.com>
2023-12-21 22:06:44 +0300

    first commit

---
+++ utils/set_commands.py
@@ -0,0 +1,25 @@
+from aiogram import Bot
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+
+async def set_commands(bot: Bot):
+    commands = [
+        BotCommand(
+            command='start',
+            description='Начало работы'
+        ),
+        BotCommand(
+            command='add_transaction',
+            description='Добавить транзакцию'
+        ),
+        BotCommand(
+            command='add_actual_balance',
+            description='Задать фактический остаток'
+        ),
+        BotCommand(
+            command='cancel',
+            description='Отменить действие'
+        )
+    ]
+
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
